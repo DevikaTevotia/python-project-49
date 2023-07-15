3049be214a14dfeb292fb07d0a78974b6bc8ea4b
DevikaTevotia <devika@tevotia.com>
2023-07-16 01:31:18 +0300

    corrected the mistakes

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
 import random
 
+
 def is_even(number):
     return number % 2 == 0
 
-def play_game():
+
+def main():
     print("Welcome to the Brain Games!")
     name = input("May I have your name? ")
-    print("Hello, " + name + "!")
-
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
-
+    print(f"Hello, {name}!")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     correct_answers = 0
     while correct_answers < 3:
-        number = random.randint(1, 20)
-        print("Question:", number)
-        answer = input("Your answer: ")
-
-        if (is_even(number) and answer.lower() == "yes") or (not is_even(number) and answer.lower() == "no"):
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        user_answer = input("Your answer: ").lower()
+        correct_answer = "yes" if is_even(number) else "no"
+        if user_answer == correct_answer:
             print("Correct!")
             correct_answers += 1
         else:
-            correct_answer = "no" if is_even(number) else "yes"
-            print("'" + answer + "' is wrong answer ;(. Correct answer was '" + correct_answer + "'.")
-            print("Let's try again, " + name + "!")
+            print(f"'{user_answer}' is wrong answer ;(.")
+            print(f"Correct answer is '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
             return
+    print(f"Congratulations, {name}!")
 
-    print("Congratulations, " + name + "!")
 
-
-play_game()
+if __name__ == '__main__':
+    main()
